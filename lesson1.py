af1d0f1768645ed6f58e7db29ce45a5129b3abf5
mamasacovartur-byte <mamasacovartur@gmail.com>
2025-12-01 19:11:34 +0300

    lesson1

---
+++ lesson1.py
@@ -0,0 +1,16 @@
+class Car:
+
+    def __init__(self,color,made):
+        self.color = color
+        self.made = made
+
+
+
+
+car_honda = Car(color='blue',made='china')
+car_subaru = Car(color='red',made='KG')
+
+print(car_subaru.color)
+print(car_honda.color)
+car_subaru.year = 2008
+print(car_subaru.year)
